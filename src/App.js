afb9b6d0dc752bdaf080796541ec672d9b784740
juunghyun <forcomfe81@gmail.com>
2023-07-03 13:35:44 +0900

    for first commit

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
               <Route exact path="/" component={MainPage}/>
               {/*path를 바꿔 다른 페이지로 이동 가능. 전달할 props가 존재한다면, 아래의 코드를 예시로 사용*/}
               {/*<Route exact path="/info" render={() => <Info userInfo={userInfo} />} />*/}
+              {/*for first commit*/}
               <Route exact path="/test" component={TestPage}/>
           </Switch>
       </BrowserRouter>
