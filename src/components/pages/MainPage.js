023e09b8d7c58f78596e0cf1e8e9cb5903e91b48
juunghyun <forcomfe81@gmail.com>
2023-07-18 14:42:33 +0900

    font css file, banner add

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -7,7 +7,7 @@ import SearchNav from "../molecules/SearchNav";
 import Nav from "../molecules/Nav";
 import ScrollDownNav from "../molecules/ScrollDownNav";
 import styled from 'styled-components';
-
+import "../atoms/font.css";
 
 
 
@@ -30,6 +30,13 @@ const MainPage = () => {
             {/*최상단 바로 아래 메뉴 Nav*/}
             <Nav></Nav>
 
+            <MainBannerDiv>
+                <BannerText>
+                    <span class="first-line">DPS Simulator</span>
+                    <br/>
+                    <span class="second-line">시뮬레이터</span>
+                </BannerText>
+            </MainBannerDiv>
             {/*사이트 최하단 Copyright 적는 부분*/}
             <CopyrightDiv>
                 <CopyrightText>Copyright @2023 mokokoroom / contact : forcomfe81@gmail.com</CopyrightText>
@@ -48,28 +55,26 @@ const MainDiv = styled.div`
   height: 200vh;
   //width: 100vw;
   grid-template-columns: repeat(6,1fr);
-  grid-template-rows: 90px 90px auto 80px;
-  
+  grid-template-rows: 90px 90px 360px auto 80px;
 `;
 
+const MainBannerDiv = styled.div`
+  grid-row:3;
+  grid-column: 1/-1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border:1px #1d1d1d solid;
+  border-top:none;
+`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+const BannerText = styled.div`
+  text-align: center;
+`;
 
 //페이지 최하단 copyright 작성을 위한 div 스타일
 const CopyrightDiv = styled.div`
-  grid-row: 4;
+  grid-row: 5;
   grid-column: 1/-1;
   display: flex;
   justify-content: center;
